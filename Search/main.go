d058ab9d9a2bd0be844f30e0be54d05ebae0555f
Siddesh-Sreeni <siddesh.tiruvadaimarudursreenivasan@sjsu.edu>
2019-11-16 16:32:32 -0800

    Search API

---
+++ Search/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/json"	
+	"fmt"
+	"log"
+	"net/http"
+	"io/ioutil"
+	"github.com/gorilla/mux"
+)	
+
+type event struct {
+	ID		string `json:"ID"`
+	Title		string `json:"Title"`
+	Description	string `json:"Description"`
+}
+
+type allEvents []event
+
+var events = allEvents{
+	{
+		ID:		"1",
+		Title:		"Dummy",
+		Description:	"Dummy db",
+	},
+}
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome Home")
+}
+
+func createEvent(w http.ResponseWriter, r *http.Request) {
+	var newEvent event
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprint(w, "Kindly enter data with the event title and description only in order to update")
+	}
+	json.Unmarshal(reqBody, &newEvent)
+	events = append(events, newEvent)
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(newEvent)
+}
+
+func getAllEvents(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(events)
+}
+
+func main() {
+//	initEvents()
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	router.HandleFunc("/event", createEvent).Methods("POST")
+	router.HandleFunc("/events", getAllEvents).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
