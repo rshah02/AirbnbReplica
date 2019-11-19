88c3422cc67175b2efd742d5882a30e3f5f59a76
rshah02 <rohankumar.shah@sjsu.edu>
2019-11-19 03:32:58 -0800

    completed profile api

---
+++ profile/src/API/main.go
@@ -0,0 +1,25 @@
+/*
+	Gumball API in Go (Version 3)
+	Uses MongoDB and RabbitMQ 
+	(For use with Kong API Key)
+*/
+
+package main
+
+import (
+	"os"
+        "fmt"
+         
+)
+
+func main() {
+           fmt.Printf("Welcome To ITzGeek\n")
+
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "3000"
+	}
+
+	server := NewServer()
+	server.Run(":" + port)
+}
