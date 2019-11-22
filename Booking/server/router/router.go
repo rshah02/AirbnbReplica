7f9f1a0f3291505c81c33ec2123dfe2cc0273a3e
MJSindhu7 <jayasindhu.manda@sjsu.edu>
2019-11-21 23:57:38 -0800

    Adding Required Headers

--- Booking/server/router/router.go
+++ Booking/server/router/router.go
@@ -1,14 +1,20 @@
 package router
 
 import (
+	"log"
+	"net/http"
 	"../middleware"
 	"github.com/gorilla/mux"
+	h "github.com/gorilla/handlers"
 )
 
 
 // Router is exported and used in main.go
 func Router() *mux.Router {
 	router := mux.NewRouter()
+	headersOk := h.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	originsOk := h.AllowedOrigins([]string{"*"})
+	methodsOk := h.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
 	router.HandleFunc("/booking", middleware.GetBookings).Methods("GET")
 	//Returns all the bookings of the passed userId
 	router.HandleFunc("/{userId}/myBookings", middleware.GetUserBookings).Methods("GET")
@@ -18,5 +24,6 @@ func Router() *mux.Router {
 	router.HandleFunc("/{userId}/updateBooking/{bookingId}", middleware.UpdateBooking).Methods("PUT")
 	//deletes a booking of that user which is passed and that passed booking
 	router.HandleFunc("/{userId}/myBookings/remove/{bookingId}", middleware.DeleteBooking).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":3000", h.CORS(headersOk, methodsOk, originsOk)(router)))
 	return router
 }
