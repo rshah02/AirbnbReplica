9565525f3fe0d7af9ca931d7158db1b37236f264
MrinaliniG <mrinalini.gopalakrishnan@sjsu.edu>
2019-11-23 08:55:01 -0800

    firebase doc

---
+++ UserAPI/models/user.go
@@ -0,0 +1,10 @@
+package models
+
+type User struct {
+	UserId string `json:"UserId" bson:"UserId" bson:"_id"`
+	Email string `json:"Email" bson:"Email"`
+    Password string	`bson:"Password" json:"Password"`
+	FirstName string  `json:"FirstName" bson:"FirstName"`
+	LastName string  `json:"LastName" bson:"LastName"`
+	Token string  `json:"token" bson:"token"`
+}
