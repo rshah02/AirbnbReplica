1258c966bb7213b5c8dcdd9c1a200f4d37d0bc53
MrinaliniG <mrinalini.gopalakrishnan@sjsu.edu>
2019-11-23 00:11:03 -0800

    docker build changes and earlier PoCs

---
+++ POC's/UserAPI-no-auth/models/user.go
@@ -0,0 +1,11 @@
+package models
+
+import "gopkg.in/mgo.v2/bson"
+
+type User struct {
+	UserId string `json:"userId" bson:"userId" bson:"_id"`
+	Email string `json:"email" bson:"email"`
+    Password string	`bson:"password" json:"password"`
+	FirstName string  `json:"firstName" bson:"firstName"`
+	LastName string  `json:"lastName" bson:"lastName"`
+}
--- UserAPI-firebase/models/user.go
+++ UserAPI-firebase/models/user.go
@@ -1,11 +1,11 @@
 package models
 
 type User struct {
-	UserId string `json:"userId" bson:"userId" bson:"_id"`
-	Email string `json:"email" bson:"email"`
-    Password string	`bson:"password" json:"password"`
-	FirstName string  `json:"firstName" bson:"firstName"`
-	LastName string  `json:"lastName" bson:"lastName"`
+	UserId string `json:"UserId" bson:"UserId" bson:"_id"`
+	Email string `json:"Email" bson:"Email"`
+    Password string	`bson:"Password" json:"Password"`
+	FirstName string  `json:"FirstName" bson:"FirstName"`
+	LastName string  `json:"LastName" bson:"LastName"`
 }
 
 type UserToken struct {
